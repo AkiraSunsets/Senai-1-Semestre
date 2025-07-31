b6f180153f673f3069e5c147b029c784469ee591
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-07-31 13:31:49 -0300

    Delete Programação Back-End/PBE_Akira-main directory

--- Programação Back-End/PBE_Akira-main/Atividade 7/Num_letras.py
+++
@@ -1,7 +0,0 @@
-print("quantidade de letras (python,paralelepipido)")
-def letter(word):
-    print(f"A palavra {word} possui {len(word)} letras.")
-
-letter("casa")
-letter("paralelepipido")
-letter("python")
